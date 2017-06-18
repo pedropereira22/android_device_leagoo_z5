cae7dd9751ad1ce4e8cc8a97f08b92b4353d00af
Pedro Pereira <pedropereirex@gmail.com>
2017-06-18 10:11:57 +0100

    add AOSPA configuration file

---
+++ pa_z5.mk
@@ -0,0 +1,17 @@
+## Specify phone tech before including full_phone
+
+# Release name
+PRODUCT_RELEASE_NAME := z5
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/leagoo/z5/device_z5.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := z5
+PRODUCT_NAME := pa_z5
+PRODUCT_BRAND := LEAGOO
+PRODUCT_MODEL := Z5
+PRODUCT_MANUFACTURER := LEAGOO
